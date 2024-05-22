d15c56f9da0663003aae9ef22f5624b18acabfbb
OlegLogunov <164496716+OlegLogunov@users.noreply.github.com>
2024-05-22 09:50:22 +0300

    Add files via upload

--- HomeWork4Mod5.py
+++ HomeWork4Mod5.py
@@ -4,8 +4,11 @@ class Building:
     def __init__(self):
         Building.total += 1
 
+street = []
 for build in range(40):
-    print(f"Строение № {build+1}")
-    Building()
+    new_build = Building()
+    print(f"Создано строение класса Building № {build+1}")
+    street.append(new_build)
 
+print(40 * "-")
 print(f"Итого на улице {Building.total} строений")
